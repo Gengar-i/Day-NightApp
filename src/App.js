6df9632c3782fd2c02bae2eb9313dcc49c444bbc
Robert Swoboda <rmmswoboda@gmail.com>
2019-10-26 19:32:20 +0200

    gelocation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: null
+    };
+  }
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState ({position: position.coords.latitude});
+        console.log(position)
+      },
+      error => console.log(error)
+    );
+  }
+  
   render() {
     return (
-    <div> Hello World </div>
+    <div> Position: {this.state.position}</div>
     )
   }
 }
