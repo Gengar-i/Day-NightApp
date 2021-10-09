e4e234517810ecc6fa31962f45104fd5ba17a4e6
Gengar-i <rmmswoboda@gmail.com>
2021-10-09 16:28:13 +0200

    Updates

---
+++ precache-manifest.b49c227dd3001809a808c8adc376d4fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b8b8f46fd8143125e8ed450bf89c9d",
+    "url": "/Day-NightApp/index.html"
+  },
+  {
+    "revision": "dbf0805a44faadc97b16",
+    "url": "/Day-NightApp/static/css/main.e247e1dd.chunk.css"
+  },
+  {
+    "revision": "baf8398a68ec53393e9f",
+    "url": "/Day-NightApp/static/js/2.c777b8e3.chunk.js"
+  },
+  {
+    "revision": "dbf0805a44faadc97b16",
+    "url": "/Day-NightApp/static/js/main.8f6e4a57.chunk.js"
+  },
+  {
+    "revision": "54d5456da269668c4332",
+    "url": "/Day-NightApp/static/js/runtime-main.02e93bfd.js"
+  },
+  {
+    "revision": "2e4909fd81c272019708164f1f217f42",
+    "url": "/Day-NightApp/static/media/afternoon.2e4909fd.jpg"
+  },
+  {
+    "revision": "36f86f0de3494476a6ee447b7ae59741",
+    "url": "/Day-NightApp/static/media/evening.36f86f0d.jpg"
+  },
+  {
+    "revision": "b7c6eae1bf2f5bffbf651dc269a63738",
+    "url": "/Day-NightApp/static/media/morning.b7c6eae1.jpg"
+  },
+  {
+    "revision": "02fdad2118fb3a99329983f7f354d55e",
+    "url": "/Day-NightApp/static/media/night.02fdad21.jpg"
+  }
+]);
